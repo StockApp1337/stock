04f72d6a1d344c5b067f97a2887d8fbafec8485c
StockApp1337 <stockapp1337@gmail.com>
2025-07-29 00:22:17 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -27,7 +27,7 @@ function main() {
       loadingTask.promise.then(function(pdf) {
         console.log("PDF Loaded");
         console.log(pdf);
-        for(var i = 0; i <= pdf.numPages; i++){
+        for(var i = 1; i <= pdf.numPages; i++){
           pdf.getPage(i).then(function(page){
             console.log(page);
         })
