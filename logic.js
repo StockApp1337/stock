72b339989275c3aa85b5cec9c16ac279d7bfc478
StockApp1337 <stockapp1337@gmail.com>
2025-07-28 22:36:46 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -5,7 +5,6 @@ function main() {
   var picker = document.getElementById("picker");
   console.log(picker);
   picker.addEventListener("change", (e)=> {
-    console.log(process.argv[2]);
     console.log(e.target.files[0]);
     console.log("inside function");
     console.log(window.location.origin);
