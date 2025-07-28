0664cf07468e4d123da37ed30894a3f259c90620
StockApp1337 <stockapp1337@gmail.com>
2025-07-28 21:27:24 +0100

    Picker
    Picker

--- logic.js
+++ logic.js
@@ -2,4 +2,9 @@ window.addEventListener("load", main());
 
 function main() {
   console.log("done");
+  var picker = document.getElementById("picker");
+  picker.addEventListener("change", (element)=> {
+    console.log(element);
+  }
+  )
 }
