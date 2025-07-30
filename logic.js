56995e448434ab93d018098adccc51d0f3f7fe7b
StockApp1337 <stockapp1337@gmail.com>
2025-07-30 04:59:26 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -44,23 +44,24 @@ function main() {
 
             } */
               console.log(databaseArray, "databaseArrayOutsideLoop");
-              for(let q = 0; q < databaseArray.length; q+=3){
-              if(cleanedArray[q].str == "Purchase Name" || cleanedArray[q].str == "Unit" || cleanedArray[q].str == "Theoretical" || cleanedArray[q].str == "Close Qty" || cleanedArray[q].str == "Closing Quantity" || cleanedArray[q].str == "TOTAL" || cleanedArray[q].str == " " || cleanedArray[q].str == "11 Bottled Cider" || cleanedArray[q].str == "12 Bottled Lager" || cleanedArray[q].str == "12A NA Bottled Lager" || cleanedArray[q].str == "13 Bottled Spirit" || cleanedArray[q].str == "14 Shots" || cleanedArray[q].str == "15 Fortified" || cleanedArray[q].str == "17 Spirits" || cleanedArray[q].str == "17A NA Spirits" || cleanedArray[q].str == "19 Cocktail Glasses" || cleanedArray[q].str == "24 Wines" || cleanedArray[q].str == "25 Sparkling" || cleanedArray[q].str == "26 Mins - Energy" || cleanedArray[q].str == "27 Mins - Mixers" || cleanedArray[q].str == "28 Mins - Packaged" || cleanedArray[q].str == "29 Mins - Non Pack" || cleanedArray[q].str == "33 Bar Garnish" || cleanedArray[q].str == "Dry Recipe Line") {
-                q++;
-                continue;
-              }
-              let tempEntry = {"Unit" : databaseArray[q+1].str,
-                               "Theoretical Close Quantity" : databaseArray[q+2].str,
-                               "Closing Quantity" : databaseArray[q+3].str,
-                               "Total" : 0};
-              database[cleanedArray[q].str] = tempEntry;
-              //console.log("database", database);
-            }
+
         })
 
-        }
       })
+      }
+      for(let q = 0; q < databaseArray.length; q+=3){
+      if(cleanedArray[q].str == "Purchase Name" || cleanedArray[q].str == "Unit" || cleanedArray[q].str == "Theoretical" || cleanedArray[q].str == "Close Qty" || cleanedArray[q].str == "Closing Quantity" || cleanedArray[q].str == "TOTAL" || cleanedArray[q].str == " " || cleanedArray[q].str == "11 Bottled Cider" || cleanedArray[q].str == "12 Bottled Lager" || cleanedArray[q].str == "12A NA Bottled Lager" || cleanedArray[q].str == "13 Bottled Spirit" || cleanedArray[q].str == "14 Shots" || cleanedArray[q].str == "15 Fortified" || cleanedArray[q].str == "17 Spirits" || cleanedArray[q].str == "17A NA Spirits" || cleanedArray[q].str == "19 Cocktail Glasses" || cleanedArray[q].str == "24 Wines" || cleanedArray[q].str == "25 Sparkling" || cleanedArray[q].str == "26 Mins - Energy" || cleanedArray[q].str == "27 Mins - Mixers" || cleanedArray[q].str == "28 Mins - Packaged" || cleanedArray[q].str == "29 Mins - Non Pack" || cleanedArray[q].str == "33 Bar Garnish" || cleanedArray[q].str == "Dry Recipe Line") {
+        q++;
+        continue;
+      }
+      let tempEntry = {"Unit" : databaseArray[q+1].str,
+                       "Theoretical Close Quantity" : databaseArray[q+2].str,
+                       "Closing Quantity" : databaseArray[q+3].str,
+                       "Total" : 0};
+      database[cleanedArray[q].str] = tempEntry;
+      //console.log("database", database);
     }
-  })
+    })
+  }
 
   }
